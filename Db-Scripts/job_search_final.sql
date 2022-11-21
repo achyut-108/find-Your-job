d3e0d9e5f9f4dda0c29226de1abb4818a76ec532
Siddhartha <siddhartha.k@greyorange.com>
2022-11-21 22:56:52 +0530

    job search query

--- Db-Scripts/job_search_final.sql
+++ Db-Scripts/job_search_final.sql
@@ -1,8 +1,10 @@
--- MySQL dump 10.13  Distrib 8.0.23, for Win64 (x86_64)
+CREATE DATABASE  IF NOT EXISTS `job_search` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `job_search`;
+-- MySQL dump 10.13  Distrib 8.0.31, for macos12 (x86_64)
 --
--- Host: 127.0.0.1    Database: job_search
+-- Host: localhost    Database: job_search
 -- ------------------------------------------------------
--- Server version	5.7.33-log
+-- Server version	8.0.31
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -23,19 +25,19 @@ DROP TABLE IF EXISTS `company_job_details`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `company_job_details` (
-  `job_details_id` int(11) NOT NULL AUTO_INCREMENT,
-  `job_id` int(11) NOT NULL,
-  `salary_per_hour` int(11) NOT NULL,
+  `job_details_id` int NOT NULL AUTO_INCREMENT,
+  `job_id` int NOT NULL,
+  `salary_per_hour` int DEFAULT NULL,
   `skills_required` text NOT NULL,
   `active` char(1) NOT NULL,
   `job_location` varchar(100) NOT NULL,
   `creation_date` datetime NOT NULL,
   `end_date` datetime DEFAULT NULL,
-  `ctc_offered` int(11) DEFAULT NULL,
+  `ctc_offered` int DEFAULT NULL,
   PRIMARY KEY (`job_details_id`),
   KEY `job_id_fk_idx` (`job_id`),
-  CONSTRAINT `job_id_fk` FOREIGN KEY (`job_id`) REFERENCES `company_jobs` (`job_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  CONSTRAINT `job_id_fk` FOREIGN KEY (`job_id`) REFERENCES `company_jobs` (`job_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -44,6 +46,7 @@ CREATE TABLE `company_job_details` (
 
 LOCK TABLES `company_job_details` WRITE;
 /*!40000 ALTER TABLE `company_job_details` DISABLE KEYS */;
+INSERT INTO `company_job_details` VALUES (1,1,NULL,'JAVA,RDBMS,SPRING BOOT,DAT STRUCTIRES AND ALGORITHMS','Y','remote','2022-11-12 21:38:14',NULL,2500000);
 /*!40000 ALTER TABLE `company_job_details` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -55,18 +58,19 @@ DROP TABLE IF EXISTS `company_jobs`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `company_jobs` (
-  `job_id` int(11) NOT NULL AUTO_INCREMENT,
-  `company_id` int(11) NOT NULL,
+  `job_id` int NOT NULL AUTO_INCREMENT,
+  `company_id` int NOT NULL,
   `job_title` varchar(200) NOT NULL,
   `job_description` text NOT NULL,
-  `job_type_id` int(11) NOT NULL,
+  `job_type_id` int NOT NULL,
   `active` char(1) NOT NULL,
+  `posted_by` int NOT NULL,
   PRIMARY KEY (`job_id`),
   KEY `company_id_fk_idx` (`company_id`),
   KEY `job_type_id_fk_idx` (`job_type_id`),
-  CONSTRAINT `company_id_fk` FOREIGN KEY (`company_id`) REFERENCES `m_company` (`company_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `job_type_id_fk` FOREIGN KEY (`job_type_id`) REFERENCES `m_job_type` (`job_type_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  CONSTRAINT `company_id_fk` FOREIGN KEY (`company_id`) REFERENCES `m_company` (`company_id`),
+  CONSTRAINT `job_type_id_fk` FOREIGN KEY (`job_type_id`) REFERENCES `m_job_type` (`job_type_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -75,7 +79,36 @@ CREATE TABLE `company_jobs` (
 
 LOCK TABLES `company_jobs` WRITE;
 /*!40000 ALTER TABLE `company_jobs` DISABLE KEYS */;
+INSERT INTO `company_jobs` VALUES (1,1001,'abc','java developer',1,'N',2),(2,1001,'bcd','angular developer',1,'Y',2);
 /*!40000 ALTER TABLE `company_jobs` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `job_application_history`
+--
+
+DROP TABLE IF EXISTS `job_application_history`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `job_application_history` (
+  `job_application_id` int NOT NULL AUTO_INCREMENT,
+  `job_id` int NOT NULL,
+  `job_seeker_id` int NOT NULL,
+  `creation_date` datetime NOT NULL,
+  `active` varchar(1) NOT NULL,
+  PRIMARY KEY (`job_application_id`),
+  UNIQUE KEY `job_applicaton_id_UNIQUE` (`job_application_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `job_application_history`
+--
+
+LOCK TABLES `job_application_history` WRITE;
+/*!40000 ALTER TABLE `job_application_history` DISABLE KEYS */;
+INSERT INTO `job_application_history` VALUES (1,1,1,'2022-11-15 16:00:16','Y');
+/*!40000 ALTER TABLE `job_application_history` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -86,14 +119,14 @@ DROP TABLE IF EXISTS `m_company`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `m_company` (
-  `company_id` int(11) NOT NULL AUTO_INCREMENT,
+  `company_id` int NOT NULL AUTO_INCREMENT,
   `company_name` varchar(150) NOT NULL,
   `company_description` varchar(500) DEFAULT NULL,
-  `user_id` int(11) NOT NULL,
+  `user_id` int NOT NULL,
   PRIMARY KEY (`company_id`),
   UNIQUE KEY `company_name_UNIQUE` (`company_name`),
   UNIQUE KEY `user_id_UNIQUE` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=1002 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -102,6 +135,7 @@ CREATE TABLE `m_company` (
 
 LOCK TABLES `m_company` WRITE;
 /*!40000 ALTER TABLE `m_company` DISABLE KEYS */;
+INSERT INTO `m_company` VALUES (1001,'XYZ PVT LTD','TELECOM SECTOR',2);
 /*!40000 ALTER TABLE `m_company` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -113,7 +147,7 @@ DROP TABLE IF EXISTS `m_degree`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `m_degree` (
-  `degree_id` int(11) NOT NULL AUTO_INCREMENT,
+  `degree_id` int NOT NULL AUTO_INCREMENT,
   `degree` varchar(150) NOT NULL,
   `degree_description` varchar(500) DEFAULT NULL,
   PRIMARY KEY (`degree_id`),
@@ -138,7 +172,7 @@ DROP TABLE IF EXISTS `m_institution`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `m_institution` (
-  `institution_id` int(11) NOT NULL AUTO_INCREMENT,
+  `institution_id` int NOT NULL AUTO_INCREMENT,
   `institution_name` varchar(200) NOT NULL,
   `address` varchar(150) NOT NULL,
   `pin_code` varchar(20) NOT NULL,
@@ -165,12 +199,12 @@ DROP TABLE IF EXISTS `m_job_type`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `m_job_type` (
-  `job_type_id` int(11) NOT NULL AUTO_INCREMENT,
+  `job_type_id` int NOT NULL AUTO_INCREMENT,
   `job_type` varchar(50) NOT NULL,
   `job_type_description` varchar(200) DEFAULT NULL,
   PRIMARY KEY (`job_type_id`),
   UNIQUE KEY `job_type_UNIQUE` (`job_type`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -179,6 +213,7 @@ CREATE TABLE `m_job_type` (
 
 LOCK TABLES `m_job_type` WRITE;
 /*!40000 ALTER TABLE `m_job_type` DISABLE KEYS */;
+INSERT INTO `m_job_type` VALUES (1,'FULL-TIME','Permanent Full Time Oppourtunity'),(2,'CONTRACTUAL','Contractual Oppourtunity'),(3,'INTERSHIP','Intern Oppourtunity');
 /*!40000 ALTER TABLE `m_job_type` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -190,7 +225,7 @@ DROP TABLE IF EXISTS `m_user_role`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `m_user_role` (
-  `role_id` int(11) NOT NULL AUTO_INCREMENT,
+  `role_id` int NOT NULL AUTO_INCREMENT,
   `role` varchar(45) NOT NULL,
   `role_desc` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`role_id`),
@@ -216,7 +251,7 @@ DROP TABLE IF EXISTS `m_user_type`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `m_user_type` (
-  `user_type_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_type_id` int NOT NULL AUTO_INCREMENT,
   `user_type` varchar(45) NOT NULL,
   PRIMARY KEY (`user_type_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
@@ -240,13 +275,13 @@ DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user` (
-  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL AUTO_INCREMENT,
   `first_name` varchar(45) NOT NULL,
   `last_name` varchar(45) DEFAULT NULL,
   `email` varchar(100) NOT NULL,
-  `user_type_id` int(11) NOT NULL,
+  `user_type_id` int NOT NULL,
   `password` varchar(200) NOT NULL,
-  `login_attempt` int(11) DEFAULT NULL,
+  `login_attempt` int DEFAULT NULL,
   `mobile_number` varchar(45) NOT NULL,
   `active` varchar(1) NOT NULL,
   `login_id` varchar(45) NOT NULL,
@@ -256,8 +291,8 @@ CREATE TABLE `user` (
   UNIQUE KEY `mobile_number_UNIQUE` (`mobile_number`),
   UNIQUE KEY `login_id_UNIQUE` (`login_id`),
   KEY `user_type_fk_idx` (`user_type_id`),
-  CONSTRAINT `USER_TYPE_ID_FK` FOREIGN KEY (`user_type_id`) REFERENCES `m_user_type` (`user_type_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+  CONSTRAINT `USER_TYPE_ID_FK` FOREIGN KEY (`user_type_id`) REFERENCES `m_user_type` (`user_type_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -266,7 +301,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'Siddhartha','Kumar','siddharthakumar4209@gmail.com',1,'76543',NULL,'+918130828881','Y','RERERER');
+INSERT INTO `user` VALUES (1,'Siddhartha','Kumar','siddharthakumar4209@gmail.com',1,'password',NULL,'+918130828881','Y','sid'),(2,'RECTUITER',NULL,'recruiter@gmail.com',2,'password',NULL,'+916289447928','Y','recruiter');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -278,8 +313,8 @@ DROP TABLE IF EXISTS `user_details`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user_details` (
-  `user_id` int(11) NOT NULL,
-  `user_role_id` int(11) NOT NULL,
+  `user_id` int NOT NULL,
+  `user_role_id` int DEFAULT NULL,
   `skills` varchar(5000) NOT NULL,
   `profile_summary` text NOT NULL,
   `address` varchar(45) NOT NULL,
@@ -290,9 +325,7 @@ CREATE TABLE `user_details` (
   `martial_status` varchar(15) NOT NULL,
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `USER_ID_UNIQUE` (`user_id`),
-  UNIQUE KEY `user_role_id_UNIQUE` (`user_role_id`),
-  CONSTRAINT `user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `user_role_fk` FOREIGN KEY (`user_role_id`) REFERENCES `m_user_role` (`role_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -302,6 +335,7 @@ CREATE TABLE `user_details` (
 
 LOCK TABLES `user_details` WRITE;
 /*!40000 ALTER TABLE `user_details` DISABLE KEYS */;
+INSERT INTO `user_details` VALUES (1,NULL,'java','I am a java developer','string','string','Muzaffarpur','1991-11-17 17:36:40','Male','string');
 /*!40000 ALTER TABLE `user_details` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -313,10 +347,10 @@ DROP TABLE IF EXISTS `user_education_history`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user_education_history` (
-  `user_education_id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) NOT NULL,
-  `degree_id` int(11) NOT NULL,
-  `institution_id` int(11) DEFAULT NULL,
+  `user_education_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `degree_id` int NOT NULL,
+  `institution_id` int DEFAULT NULL,
   `major` varchar(45) NOT NULL,
   `start_date` datetime DEFAULT NULL,
   `end_date` datetime DEFAULT NULL,
@@ -325,9 +359,9 @@ CREATE TABLE `user_education_history` (
   KEY `user_education_user_id_fk_idx` (`user_id`),
   KEY `user_degree_fk_idx` (`degree_id`),
   KEY `user_institution_fk_idx` (`institution_id`),
-  CONSTRAINT `user_degree_fk` FOREIGN KEY (`degree_id`) REFERENCES `m_degree` (`degree_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `user_education_user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `user_institution_fk` FOREIGN KEY (`institution_id`) REFERENCES `m_institution` (`institution_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `user_degree_fk` FOREIGN KEY (`degree_id`) REFERENCES `m_degree` (`degree_id`),
+  CONSTRAINT `user_education_user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`),
+  CONSTRAINT `user_institution_fk` FOREIGN KEY (`institution_id`) REFERENCES `m_institution` (`institution_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -348,8 +382,8 @@ DROP TABLE IF EXISTS `user_employment_history`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user_employment_history` (
-  `user_employment_id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) NOT NULL,
+  `user_employment_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
   `designation` varchar(50) NOT NULL,
   `employer` varchar(50) DEFAULT NULL,
   `start_date` datetime DEFAULT NULL,
@@ -359,7 +393,7 @@ CREATE TABLE `user_employment_history` (
   PRIMARY KEY (`user_employment_id`),
   UNIQUE KEY `user_employment_id_UNIQUE` (`user_employment_id`),
   KEY `user_id_employment_fk_idx` (`user_id`),
-  CONSTRAINT `user_id_employment_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `user_id_employment_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -380,15 +414,15 @@ DROP TABLE IF EXISTS `user_project_history`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user_project_history` (
-  `project_id` int(11) NOT NULL AUTO_INCREMENT,
-  `employement_id` int(11) NOT NULL,
+  `project_id` int NOT NULL AUTO_INCREMENT,
+  `employement_id` int NOT NULL,
   `start_date` datetime DEFAULT NULL,
   `end_date` datetime DEFAULT NULL,
   `project_description` text,
   `project_name` varchar(45) NOT NULL,
   PRIMARY KEY (`project_id`),
   KEY `project_employment_fk_idx` (`employement_id`),
-  CONSTRAINT `project_employment_fk` FOREIGN KEY (`employement_id`) REFERENCES `user_employment_history` (`user_employment_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `project_employment_fk` FOREIGN KEY (`employement_id`) REFERENCES `user_employment_history` (`user_employment_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -410,4 +444,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-03-16 18:48:56
+-- Dump completed on 2022-11-21 10:41:50
